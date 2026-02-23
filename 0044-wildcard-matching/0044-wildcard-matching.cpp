b9a711fffdefa0ab2fbf3154b1dcb85b9abde545
Ganeshpatil-ash <akadhant@gmail.com>
2026-02-23 22:55:01 +0530

    Time: 0 ms (100%), Space: 9.8 MB (88.73%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -0,0 +1,36 @@
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int i = 0, j = 0;
+        int star = -1, match = 0;
+
+        while (i < s.length()) {
+            if (j < p.length() && (p[j] == s[i] || p[j] == '?')) {
+                i++;
+                j++;
+            }
+            else if (j < p.length() && p[j] == '*') {
+                star = j;
+                match = i;
+                j++;
+            }
+            else if (star != -1) {
+                j = star + 1;
+                match++;
+                i = match;
+            }
+            else {
+                return false;
+            }
+        }
+
+        while (j < p.length() && p[j] == '*') {
+            j++;
+        }
+
+        return j == p.length();
+    }
+};
