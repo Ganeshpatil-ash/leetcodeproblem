0953ce87bf540cfbda3b0b0838b3eb266a09922b
Ganeshpatil-ash <akadhant@gmail.com>
2026-07-28 17:29:07 +0530

    Time: 0 ms (100%), Space: 30.5 MB (18.76%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,22 +2,14 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        k %= n;
 
-        vector<int> temp(k);
-
-        // step 1: store last k elements
-        for (int i = 0; i < k; i++) {
-            temp[i] = nums[n - k + i];
+        k = k%n;
+        vector <int> temp(n);
+        for (int i = 0; i < n; i++){
+            temp[(i+k)%n] = nums[i];
         }
 
-        // step 2: shift remaining to the right
-        for (int i = n - k - 1; i >= 0; i--) {
-            nums[i + k] = nums[i];
-        }
-
-        // step 3: put temp elements at front
-        for (int i = 0; i < k; i++) {
+        for (int i = 0; i < n; i++){
             nums[i] = temp[i];
         }
     }
