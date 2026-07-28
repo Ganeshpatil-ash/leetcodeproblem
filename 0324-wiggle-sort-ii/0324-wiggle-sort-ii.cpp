8ef75964a1b375c4a1de50bbbda25436d1da374a
Ganeshpatil-ash <akadhant@gmail.com>
2026-07-28 18:39:27 +0530

    Time: 0 ms (100%), Space: 21.8 MB (87.67%) - LeetHub

---
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        int n = nums.size();
+        vector <int> temp = nums;
+        sort (temp.begin(),temp.end());
+        int small = (n-1)/2 ;
+         int large = n-1;
+        for(int i = 0; i < n; i++){
+            if(i%2 == 0){
+                nums[i] = temp[small--];
+            }
+            else{
+                nums[i] = temp[large--];
+            }
+        }
+    }
+};
